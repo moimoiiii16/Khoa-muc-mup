cfa8a9182bf7e389599b2ac043f70019696935cb
Khoaph1709 <khoa555.rubik@gmail.com>
2025-02-19 19:16:17 +0700

    Create Week4
    lkjfslksdjkfldsa;jfkdsaklfdsf

---
+++ Week4.java
@@ -0,0 +1,32 @@
+public class Week4 {
+    public static int max2Int(int a, int b) {
+	// Tim gia tri lon nhat cua hai so nguyen
+        return a > b ? a : b;
+    }
+
+    public static int minArray (int[] array) {
+	// Tim gia tri nho nhat cua 1 mang so nguyen
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static String calculateBMI(double weight, double height){
+	// Tinh BMI
+        double bmi = weight / (height * height);
+        if (bmi < 18.5) {
+            return "Thiếu cân";
+        } else if (bmi >= 18.5 && bmi < 23) {
+            return "Bình thường";
+        } else if (bmi >= 23 && bmi < 24.99) {
+            return "Thừa cân";
+        } else {
+            return "Béo phì";
+        }
+    }
+}
+ 
---
+++ Week4Test.java
@@ -0,0 +1,80 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class Week4Test {
+    @Test
+    public void testMax2Int1(){
+        assertEquals(5, Week4.max2Int(5, 3));
+        assertEquals(5, Week4.max2Int(3, 5));
+        assertEquals(5, Week4.max2Int(5, 5));
+        assertEquals(0, Week4.max2Int(0, 0));
+        assertEquals(-5, Week4.max2Int(-5, -10));
+        assertEquals(-5, Week4.max2Int(-10, -5));
+        assertEquals(0, Week4.max2Int(-5, 0));
+        assertEquals(0, Week4.max2Int(0, -5));
+        assertEquals(0, Week4.max2Int(0, 0));
+    }
+
+    @Test
+    public void testMax2Int2(){
+        assertEquals(5, Week4.max2Int(5, 3));
+        assertEquals(5, Week4.max2Int(3, 5));
+        assertEquals(5, Week4.max2Int(5, 5));
+        assertEquals(0, Week4.max2Int(0, 0));
+        assertEquals(-5, Week4.max2Int(-5, -10));
+        assertEquals(-5, Week4.max2Int(-10, -5));
+        assertEquals(0, Week4.max2Int(-5, 0));
+        assertEquals(0, Week4.max2Int(0, -5));
+        assertEquals(0, Week4.max2Int(0, 0));
+    }
+
+    @Test
+    public void testMax2Int3(){
+        assertEquals(5, Week4.max2Int(5, 3));
+        assertEquals(5, Week4.max2Int(3, 5));
+        assertEquals(5, Week4.max2Int(5, 5));
+        assertEquals(0, Week4.max2Int(0, 0));
+        assertEquals(-5, Week4.max2Int(-5, -10));
+        assertEquals(-5, Week4.max2Int(-10, -5));
+        assertEquals(0, Week4.max2Int(-5, 0));
+        assertEquals(0, Week4.max2Int(0, -5));
+        assertEquals(0, Week4.max2Int(0, 0));
+    }
+
+    @Test
+    public void testMax2Int4(){}
+
+    @Test
+    public void testMax2Int5(){}
+
+    @Test
+    public void testMinArray1(){}
+
+    @Test
+    public void testMinArray2(){}
+
+    @Test
+    public void testMinArray3(){}
+
+    @Test
+    public void testMinArray4(){}
+
+    @Test
+    public void testMinArray5(){}
+
+    @Test
+    public void testCalculateBMI1(){}
+
+    @Test
+    public void testCalculateBMI2(){}
+
+    @Test
+    public void testCalculateBMI3(){}
+
+    @Test
+    public void testCalculateBMI4(){}
+
+    @Test
+    public void testCalculateBMI5(){}
+} 
